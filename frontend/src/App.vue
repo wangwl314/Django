3ea0d87d30502fc18a7766bcb0736b9af585f006
wangwl314 <arkathz@163.com>
2020-10-15 17:36:04 +0800

    latest

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/login">login</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <testcomp></testcomp>
+  </div>
+</template>
+
+<script>
+import testcomp from "../components/testcomp"
+
+export default {
+  name: "Home",
+  components: {
+    testcomp,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
